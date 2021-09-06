20f2d1e718f346149b1ef10593e719ea3547adef
AaronCore <969340992@qq.com>
2021-09-06 16:11:36 +0800

    commit

---
+++ 1-vue2/2_test/02_src_ref属性/components/School.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="school">
+    <h2>学校名称：{{ name }}</h2>
+    <h2>学校地址：{{ address }}</h2>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'School',
+  data() {
+    return {
+      name: '深圳大学',
+      address: '南山区'
+    }
+  },
+}
+</script>
+
+<style>
+.school {
+  background-color: gray;
+}
+</style>
