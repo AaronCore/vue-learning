43778f4468a7e8165c86bba117d21eaa20efa940
AaronCore <969340992@qq.com>
2021-09-23 14:01:37 +0800

    commit

--- 1-vue2/2_test/22_插槽/3_src_作用域插槽/App.vue
+++ 1-vue2/2_test/22_插槽/3_src_作用域插槽/App.vue
@@ -4,7 +4,7 @@
     <Category title="游戏">
       <template scope="atguigu">
         <ul>
-          <li v-for="(g,index) in atguigu.games" :key="index">{{ g }}</li>
+             <li v-for="(g,index) in atguigu.games" :key="index">{{ g }}</li>
         </ul>
       </template>
     </Category>
